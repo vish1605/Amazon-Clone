92d7431fb377ef15026908654e9ef516b7507fe5
vishal <vishalthutte1605@gmail.com>
2023-12-07 15:59:41 +0530

    Fixed the Radio Selector option

--- data/cart.js
+++ data/cart.js
@@ -7,7 +7,10 @@ export const cart = [{
     
     productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
     quantity: 3
-}];
+},
+{productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
+quantity: 3}
+];
 
 export function addToCart(productId) {
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -55,7 +55,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -68,7 +68,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -81,7 +81,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
