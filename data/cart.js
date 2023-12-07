500fe8f9a5ced1421b1b659ca94c5bba43717067
vishal <vishalthutte1605@gmail.com>
2023-12-07 15:36:24 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity : 2
+
+},
+{
+    
+    productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity: 3
+}];
 
 export function addToCart(productId) {
 
